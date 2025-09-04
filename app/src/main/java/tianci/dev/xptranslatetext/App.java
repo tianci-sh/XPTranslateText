de8d4a1e7736f8dc0f1cd334025e56762c336e17
vocolboy <vocolboy@gmail.com>
2025-09-05 01:39:36 +0800

    chore: remove force dark mode

--- app/src/main/java/tianci/dev/xptranslatetext/App.java
+++ app/src/main/java/tianci/dev/xptranslatetext/App.java
@@ -11,6 +11,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
